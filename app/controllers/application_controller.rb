93b3a155cc28863af61a5a57ba0e1071c499a862
robinhwang <binbinhuangcsu@gmail.com>
2013-03-02 15:03:34 +0800

    tags

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :tags
+  private
+
+  def tags
+    @tags = Tag.all
+  end
 
 end
--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,10 +1,16 @@
 class TagsController < ApplicationController
 
+  def application
+    @tags = Tag.all
+
+    render :layout => "application"
+  end
+
 	def index
     @tags = Tag.all
   end
-  
+
 	def show
-		@tag = Tag.find(params[:id])		
-	end	
+		@tag = Tag.find(params[:id])
+	end
 end
